17159a54342138a7f88ec5ae715bb661f8e65846
shubhra01 <94222495+shubhra01@users.noreply.github.com>
2024-07-21 19:14:24 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.113.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = var.bkstrgrg
+    storage_account_name = var.bkstrg
+    container_name = var.bkcontainer
+    key = var.bkstrgkey
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
